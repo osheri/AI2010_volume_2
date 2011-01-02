c3e004e95b0c80469e2559429124dca5eb0e8308
sheix <anton.sheihman@gmail.com>
2011-01-02 12:58:56 +0200

    Reading files as input; creating rooms and pieces of furniture

--- src/ai2010v2/Piece.java
+++ src/ai2010v2/Piece.java
@@ -1,5 +1,7 @@
 package ai2010v2;
 
+import sun.io.Converters;
+
 /*
  *    Piece is rectangular piece of furniture.
  *    
@@ -15,4 +17,17 @@ package ai2010v2;
 public class Piece {
 	public int x,y;
 	public int w, h;
+	public int id;
+	public char c;
+	Piece(int ID)
+	{
+		id=ID;
+		
+	}
+	public void Draw()
+	{
+		
+		System.out.println("I am piece " + id +", placed on ("+ x +","+y+"), with size of["+w +","+h+"]");
+	}
+	
 }
